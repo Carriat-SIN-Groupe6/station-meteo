439c88d39e82f1ea4c0762b693d725aa88d1f8fb
Lucas <luxxiiglitcher@gmail.com>
2025-01-31 15:12:39 +0100

    15:12/31/01

--- test api.py
+++ test api.py
@@ -5,6 +5,7 @@ from picoder import *
 
 led1 = LED(1)
 led2 = LED(2)
+bzrtone = BUZZRTONE()
 
 #Entrer les paramètres du point d'accès
 ssid = 'WIFI-SIN'
@@ -17,16 +18,22 @@ wlan.config(pm = 0xa11140)              # désactive le mode power-save
 wlan.connect(ssid, password)
 
 while True:
-    if wlan.status() = 3:
+    if wlan.status() == 3:
         
         led1.on()
         led2.on()
-        res = requests.get(url='http://www.baidu.com/')
+        res = requests.get(url='https://api.open-meteo.com/v1/forecast?latitude=46.197376&longitude=5.223058&current=temperature_2m,precipitation&hourly=temperature_2m,precipitation,wind_speed_10m&timezone=auto')
         print(res.text)
         time.sleep(3)
         led1.off()
         led2.off()
+        break
         
     elif wlan.status() < 0 or wlan.status() >= 3:
-        led1.on()
-        led2.off()
+        for i in range(3):
+            led1.on()
+            sleep(0.01)
+            led2.off()
+            sleep(0.01)
+            bzrtone.play(50,5000)
+            sleep(0.01)
